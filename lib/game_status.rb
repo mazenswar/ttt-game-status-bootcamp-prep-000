0dc1962a1db1cf7716c46400de22a0e3b8c408f5
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-19 01:39:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,36 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
+WIN_COMBINATIONS = [
+  [0, 1, 2], #top row
+  [3, 4, 5], #middle row
+  [6, 7, 8], #bottom row
+  [0, 3, 6], #left column
+  [1, 4, 7], #middle column
+  [2, 5, 8], #right column
+  [2, 4, 6], #diagonal right to left
+  [0, 4, 8]  #diagonal left to right
+  ]
+  
+def won(board)
+  
+  WIN_COMBINATIONS.each? do |comb|
+    if comb[0] == "X" && comb[1] == "X" &&
+    comb[2] == "X"
+    "Top row win!"
+  
+  if WIN_COMBINATIONS[0][0] == "X" && WIN_COMBINATIONS[0][1] == "X" &&
+    WIN_COMBINATIONS[0][2] == "X"
+    "Top row win!"
+  elsif WIN_COMBINATIONS[1][0] == "X" && WIN_COMBINATIONS[1][1] == "X" &&
+    WIN_COMBINATIONS[1][2] == "X"
+    "Middle row win!"
+  elsif WIN_COMBINATIONS[2][0] == "X" && WIN_COMBINATIONS[2][1] == "X" &&
+    WIN_COMBINATIONS[2][2] == "X"
+    "Top row win!"
+  
+  
+  
+  
+end
