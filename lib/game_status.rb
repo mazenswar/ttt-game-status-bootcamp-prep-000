ba3f24ef03d00a1fa350a3b22c2e857c70d93f4e
Mazen AlSwar <mazenswar@protonmail.com>
2018-10-19 01:45:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,23 +17,8 @@ WIN_COMBINATIONS = [
   ]
   
 def won(board)
-  
-  WIN_COMBINATIONS.each? do |comb|
-    if comb[0] == "X" && comb[1] == "X" &&
-    comb[2] == "X"
-    "Top row win!"
-  
-  if WIN_COMBINATIONS[0][0] == "X" && WIN_COMBINATIONS[0][1] == "X" &&
-    WIN_COMBINATIONS[0][2] == "X"
-    "Top row win!"
-  elsif WIN_COMBINATIONS[1][0] == "X" && WIN_COMBINATIONS[1][1] == "X" &&
-    WIN_COMBINATIONS[1][2] == "X"
-    "Middle row win!"
-  elsif WIN_COMBINATIONS[2][0] == "X" && WIN_COMBINATIONS[2][1] == "X" &&
-    WIN_COMBINATIONS[2][2] == "X"
-    "Top row win!"
-  
-  
-  
-  
+ WIN_COMBINATIONS.each do |comb|
+   if comb[0] == "X" && comb[1] == "X" && comb[2] == "X"
+    comb
+ 
 end
